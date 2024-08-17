70a67adfd7026652ad6cfaa4785d083a852c9f15
Siyamitt <siyamittal014@gmail.com>
2024-08-17 15:36:44 +0530

    last commit

--- GreenBasket/urls.py
+++ GreenBasket/urls.py
@@ -5,7 +5,8 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('home.urls')),
-    path('accounts/',include('accounts.urls'))
+    path('accounts/',include('accounts.urls')),
+    path('cart/',include('cart.urls'))
 
     
 ]
