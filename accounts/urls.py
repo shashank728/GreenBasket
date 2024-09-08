0aab56fe6f39a27c1c167e2371b4925000628f96
Adwait Patel <145041038+AdwaitPatel@users.noreply.github.com>
2024-09-08 10:58:09 +0530

    donebyadp

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,6 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login/',views.login,name="login"),
-    path('register/',views.register,name="register")
+    path('login/', views.login,name="login"),
+    path('register/', views.register,name="register"),
+    path('profile/', views.profile, name="profile"),
+    path('logout/', views.logout, name="logout")
 ]
+
+
+
+
+
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import auth
 
 def login(request):
     return render(request,"accounts/login.html")
+
 def register(request):
     return render(request,"accounts/signup.html")
+
+def profile(request):
+    return render(request, "accounts/profile.html")
+
+def logout(request):
+    auth.logout
+    return redirect("home")
